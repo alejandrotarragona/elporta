81436c9f1bd4c43cfcded22f30ec526abf1bdb56
alejandro <alejandro.tarragona@davinci.edu.ar>
2023-08-07 18:15:18 +0200

    Created initial project

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'serverless', // Esta es la opción que te permite desplegar tu sitio en una plataforma como Vercel o Netlify
+  images: {
+    domains: ['cdn.sanity.io', 'i.imgur.com'], // Agrega 'i.imgur.com' a la lista de dominios permitidos
+  },
 };
 
 module.exports = nextConfig;
